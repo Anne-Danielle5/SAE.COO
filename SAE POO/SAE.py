96d1abfdc55f821b38a1663315aeffdf3abed784
aminatadiallo24 <dialloamirama@gmail.com>
2024-02-29 15:29:54 +0100

    Update SAE.py

--- SAE POO/SAE.py
+++ SAE POO/SAE.py
@@ -1,6 +1,17 @@
 import csv
+import pandas as pd
+
+data = []
+
 with open('extraction-2021-2022-anonyme.csv', newline='', encoding='latin-1') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=';')
     for row in csv_reader:
-        for value in row:
-            print(value)
+        data.append(row)  # Ajouter chaque ligne du CSV à la liste data
+
+# Créer un DataFrame à partir des données
+df = pd.DataFrame(data, columns=['Reservation au nom de', 'Domaines ', 'Ressource ', 'Description ', 'Heure-Durée ', 'Type ', 'Dernière mise à jour '])
+
+print(df)
+
+
+
